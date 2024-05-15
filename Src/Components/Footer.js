57d0f52c7eb1939f2b58a77bd9234cebb71f9782
Gabriel Souza <paixaogabriel498@gmail.com>
2024-05-15 20:41:23 -0300

    Commit

--- Src/Components/Footer.js
+++ Src/Components/Footer.js
@@ -3,12 +3,36 @@
 import { Feather } from "@expo/vector-icons";
 import { Pressable, View, Text } from "react-native";
 import styles from '../../Css/Style';
+import { useState } from "react";
+import { Button } from "react-native-web";
+import { Modal } from "react-native-web";
 
 export default function Footer(){
 
+    const [btnAddVisivel, setBtnAddVisivel] = useState(false)
+
+    const MostrarBtnAdd = () => {
+        setBtnAddVisivel(true);
+    }
+
     return(
-        <Pressable style={{ position: 'absolute', bottom: '7%', left: '30%', backgroundColor: '#31654e', borderRadius: 50, zIndex: 1000}}>
-            <Feather name="plus" size={50} color='white'/>
-        </Pressable>
+        <>
+            <Pressable onPress={MostrarBtnAdd} style={{ position: 'absolute', bottom: '7%', left: '42.5%', backgroundColor: '#31654e', borderRadius: 50, zIndex: 1000}}>
+                <Feather name="plus" size={50} color='white'/>
+            </Pressable>
+
+            <Modal
+                animationType="none"
+                transparent={true}
+                visible={btnAddVisivel}
+                onRequestClose={MostrarBtnAdd}
+            >
+                <View>
+                    <Button title="Teste1" />
+                    <Button title="Teste2" />
+                    <Button title="Teste3" />
+                </View>
+            </Modal>
+        </>
     )
 }
---
+++ Src/Screens/login.js
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity, Image} from 'react-native';
+import logoImage from 'C:/Users/lucas/Documents/1 Estudos/react/WalletWise/Src/Images/logoVerde.png';
+
+const Login = ({ onLoginSuccess }) => {
+
+  const [hasLogin, setHasLogin] = useState(false);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+  const SingToLog = () =>{
+    hasLogin == false ? (setHasLogin(true)) : (setHasLogin(false))
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.centeredBox}>
+        {hasLogin ? (
+          <>
+            <Text style={styles.title}>Entrar</Text>
+            <Image source={logoImage} style={{ width: 130, height: 100}}/>
+            <TextInput style={styles.input} placeholder="Email" placeholderTextColor="#999"/>
+            <TextInput style={styles.input} placeholder="Senha" secureTextEntry placeholderTextColor="#999"/>
+            <TouchableOpacity style={styles.Button} onPress={onLoginSuccess}><Text style={{ color: '#fff' }}>Entrar</Text></TouchableOpacity>
+            <TouchableOpacity onPress={SingToLog}><Text style={{color: '#31654e', padding: 25, fontSize: 16, width: 300, textAlign: 'center',fontWeight: 'bold'}}>Não tenho cadastro</Text></TouchableOpacity>
+          </>
+        ) : (
+          <>
+            <Text style={styles.title}>Cadastrar</Text>
+            <Image source={logoImage} style={{ width: 130, height: 100}}/>
+            <TextInput textContentType='emailAddress' style={styles.input} placeholder="Email" placeholderTextColor="#999"/>
+            <TextInput style={styles.input} placeholder="Senha" secureTextEntry placeholderTextColor="#999"/>
+            <TextInput style={styles.input} placeholder="Primeiro Nome" value={firstName} onChangeText={text => setFirstName(text)} placeholderTextColor="#999"/>
+            <TextInput style={styles.input} placeholder="Segundo Nome" value={lastName} onChangeText={text => setLastName(text)} placeholderTextColor="#999"/>
+            <TouchableOpacity style={styles.Button} onPress={SingToLog}><Text style={{ color: '#fff' }}>Cadastrar</Text></TouchableOpacity>
+            <TouchableOpacity onPress={SingToLog}><Text style={{color: '#31654e', padding: 25, fontSize: 16, width: 200, textAlign: 'center', fontWeight: 'bold'}}>Já tenho cadastro</Text></TouchableOpacity>
+          </>
+        )}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#31654e',
+  },
+  centeredBox: {
+    width: 300,
+    height: 500, 
+    backgroundColor: '#fff',
+    paddingVertical: 30, 
+    paddingHorizontal: 70, 
+    borderRadius: 5,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: 0.75,
+    shadowRadius: 3.84,
+    elevation: 5,
+
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 10,
+    fontWeight: 'bold',
+  },
+  input: {
+    width: '160%',
+    paddingVertical: 7, 
+    paddingHorizontal: 10,   
+    margin: 2,
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#999',
+    borderRadius: 5,
+  },
+  Button: {
+    width: '160%',
+    backgroundColor: '#31654e',
+    paddingVertical: 7, 
+    paddingHorizontal: 10,  
+    borderRadius: 5,
+    marginTop: 30,
+    alignItems: 'center',
+  },
+});
+
+export default Login;
